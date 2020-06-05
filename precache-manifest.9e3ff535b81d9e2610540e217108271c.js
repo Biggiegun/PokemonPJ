045aa984cf51b5bd1f8c1e06635bf58d00e4c7e5
David Andrés <biggiegun@gmail.com>
2020-06-04 23:17:05 -0500

    Updates

---
+++ precache-manifest.9e3ff535b81d9e2610540e217108271c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cde17b3c4f3965634c4be6014a94e2",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "824b2e85a1e7155a71cc",
+    "url": "/repository-name/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "808f3acf116f9dc22597",
+    "url": "/repository-name/static/css/main.cba82f03.chunk.css"
+  },
+  {
+    "revision": "824b2e85a1e7155a71cc",
+    "url": "/repository-name/static/js/2.03c3b323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.03c3b323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808f3acf116f9dc22597",
+    "url": "/repository-name/static/js/main.1f41378b.chunk.js"
+  },
+  {
+    "revision": "ea8c6db36afa546c7f49",
+    "url": "/repository-name/static/js/runtime-main.4ae30fe0.js"
+  }
+]);
